8ed9dd106fe0ea2fee11a9a5fab18f703fe8d317
Julia Ivanova <jeivano91@gmail.com>
2020-04-24 18:22:20 +0300

    Updates

---
+++ precache-manifest.3ca574f8d8d548f12cf7b2148481e453.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71b2e3436d098b1069721aeaa4a1f6c8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1ce308c276ae09e6e0f7",
+    "url": "/monsters-rolodex/static/css/main.c76edf81.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ce308c276ae09e6e0f7",
+    "url": "/monsters-rolodex/static/js/main.fa5f6e44.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
